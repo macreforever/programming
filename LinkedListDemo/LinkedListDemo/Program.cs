55ddb7d069e4946fed8d80e5976e807462109f80
macreforever <31043628+macreforever@users.noreply.github.com>
2019-05-30 17:55:12 -0500

    Add files via upload

---
+++ LinkedListDemo/LinkedListDemo/Program.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDemo
+{
+
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        //Constructor of the class Node
+        public Node(int i)
+        {
+            data = i;
+            next = null;
+        }
+
+        
+
+        public void Print()
+        {
+            //Print all nodes
+            Console.Write("|" + data + "|->");
+            if (next != null)
+            {
+                next.Print();
+            }
+        }
+
+        public void AddToEnd(int data)
+        {
+            //Add a node to the end
+            if (next == null)
+            {
+                next = new Node(data);
+            }
+            else
+            {
+                next.AddToEnd(data);
+            }
+        }
+
+        public void AddSorted(int data)
+        {
+            //Add a node in a sorted form
+            if (next == null)
+            {
+                next = new Node(data);
+            }
+            else if (data < next.data)
+            {
+                Node temp = new Node(data);
+                temp.next = this.next;
+                this.next = temp;
+            }
+            else
+            {
+                next.AddSorted(data);
+            }
+        }
+    }
+    public class MyList
+    {
+        public Node headNode;
+
+        //Constructor
+        public MyList()
+        {
+            headNode = null;
+        }
+
+        public void AddToEnd(int data)
+        {
+            //Add a new node at the end of the list
+            if (headNode == null)
+            {
+                headNode = new Node(data);
+            }
+            else
+            {
+                headNode.AddToEnd(data);
+            }
+        }
+
+        public void AddToBeginning(int data)
+        {
+            // Add a new node in the beggining of the list
+            if (headNode == null)
+            {
+                headNode = new Node(data);
+            }
+            else
+            {
+                Node temp = new Node(data);
+                temp.next = headNode;
+                headNode = temp;
+            }
+        }
+
+        public void AddSorted(int data)
+        {
+            //Add a new node in a sorted way
+
+            if (headNode == null)
+            {
+                headNode = new Node(data);
+            }
+            else if (data < headNode.data)
+            {
+                AddToBeginning(data);
+            }
+            else
+            {
+                headNode.AddSorted(data);
+            }
+        }
+
+        public void DeleteSortedNode(int data)
+        {
+            //This method delete a node
+            if(headNode == null) return;
+
+            //If the first node is deleted now the headNode is the second node pointed in headNode.next
+            if (headNode.data == data)
+            {
+                headNode = headNode.next;
+            }
+            else {
+                Node previus = headNode;
+                Node current = headNode.next;
+
+                while (current != null)
+                {
+                    if (current.data == data)
+                    {
+                        previus.next = current.next;
+                        break;
+                    }
+
+                    previus = current;
+                    current = current.next;
+                }
+            }
+           
+            
+
+        }
+
+        public void PrintH()
+        {
+            //Print all nodes 
+            if (headNode != null)
+            {
+                headNode.Print();
+            }
+        }
+
+        public void insertAfter(int prev_node, int new_data)
+        {
+            //This method insert a new node in a specific position
+            if (headNode == null) return; //if head is null return.
+            Node current = headNode;
+
+            Node new_node = new Node(new_data);
+
+            //This is in case of insert a node in the last node
+            if (current.next == null)
+            {
+                headNode.AddToEnd(new_data);
+                return;
+            }
+
+            
+            while (current.next != null)
+            {
+                //find the previous node to insert a new node.
+                if (prev_node == current.data)
+                {
+                    new_node.next = current.next;
+                    current.next = new_node;
+                }
+                current = current.next;
+            }
+
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an instance of my linked list called MyList
+            MyList list = new MyList();
+            list.AddSorted(9);
+            list.AddSorted(5);
+            list.AddSorted(7);
+            list.AddSorted(11);
+            list.PrintH();
+            Console.WriteLine();
+            //Insert an element in a specific position
+            list.insertAfter(5, 6);
+            list.PrintH();
+            Console.WriteLine();
+            //Delete a Node
+            list.DeleteSortedNode(5);
+            list.PrintH();
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ LinkedListDemo/LinkedListDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("LinkedListDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("LinkedListDemo")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("e05d77ca-c451-44da-9828-ef5ad64c0c5d")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
+// mediante el carácter '*', como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
